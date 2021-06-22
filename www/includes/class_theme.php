1bb9657cf18486d6088d48ae84c33d7ac76b9ba0
Drek <doxology@rogers.com>
2021-06-22 13:39:38 -0300

    One more kick at the gettext/getlang can.

--- www/includes/class_theme.php
+++ www/includes/class_theme.php
@@ -563,7 +563,7 @@ function language_dir($theme = null) {
 
 // Translate a string phrase, or return the original string if no translation is available.
 function trans($str, $args = array()) {
-    if (is_null($str) and empty($args)) {
+    if (empty($str)) {
         return $str;
     } else {
         return $this->lang->gettrans($str, $args);
