27fa324da117b6ad8b7544b8ad9e3dfb0be6e84a
Drek <doxology@rogers.com>
2021-07-25 19:04:51 -0300

    Add comments to sql.

--- migrations/20210724_socialclubprofile.sql
+++ migrations/20210724_socialclubprofile.sql
@@ -1,1 +1,4 @@
+/*
+:? Reason: shorten the name and have it match the other social option names
+*/
 ALTER TABLE `ps_plr_profile` CHANGE `socialclubprofile` `socialclub` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL;
