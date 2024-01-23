cd003a42032cb1c9d9241e4c666526b6dd678524
Owen Lee Stairs <llewen@OWEN.cc-lan>
2024-01-23 19:28:19 -0400

    * [Added] Switch to exclude bots from ranking.
    - includes note regarding bots in page headers

---
+++ www/install/migrations/327b_base_20240122_01_bot-ranking-switch.sql
@@ -0,0 +1,3 @@
+INSERT INTO `ps_config` (`id`, `conftype`, `section`, `var`, `value`, `label`, `type`, `locked`, `verifycodes`, `options`, `help`) 
+    VALUES 
+        (2632,'main','ranking','bots_listed','0','(BOT\'s) Rank?','boolean',0,NULL,'','Should BOT\'s be listed? This does not remove BOT stats from player stats or change ranks, it simply prevents BOT\'s from being listed in stat pages.');
